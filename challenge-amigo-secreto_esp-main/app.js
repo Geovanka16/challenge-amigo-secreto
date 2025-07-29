3fbbb075769b94313e2c7cfd38fefda0ebc78ee6
Geovanka <cuerogeovanka@gmail.com>
2025-07-29 12:25:58 -0300

    Implementa función para agregar amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,2 +1,17 @@
-let amigos = [];
+let listaDeAmigos = [];
 
+function agregarAmigo() {
+    let valorNombre = document.getElementById('amigo').value;
+
+    if (valorNombre == '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        listaDeAmigos.push(valorNombre);
+        console.log(listaDeAmigos);
+        limpiarCaja();
+    }
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
